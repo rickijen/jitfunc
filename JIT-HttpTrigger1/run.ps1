0aeb59fa0b1c3dfcaea8f33ca5151f7af7eeed61
rickijen <rickijen@gmail.com>
2021-03-22 10:49:36 -0700

    Update run.ps1
    Updated for demo

--- JIT-HttpTrigger1/run.ps1
+++ JIT-HttpTrigger1/run.ps1
@@ -14,30 +14,19 @@ if (-not $name) {
 
 ########################## VARIABLES ###############################
 # Subscription ID
-$SubscriptionId = "XXXXXXXX"
+$SubscriptionId = "7f3cc5e6-2fda-4486-b5c4-66de147a9f86"
 # User Assigned Managed Identity ID - retrieve from portal and need to run New-AzRoleAssignment -ObjectId <Obj ID of Client ID>
-$UAMIClientId = "f0897ee3-bfa6-4244-b04e-5490e19b70eb"
+$UAMIClientId = "0a2a4f78-c9d5-4e29-8823-ddc84f1c38c8"
 # Variables for REST API calls
 $Apiversion = "2020-01-01"
 $JitNetworkAccessPolicyName = "default"
-$AscLocation = "westus"
-$ResourceGroupName = "JIT-demo"
-$VMName = "VM-Target"
+$AscLocation = "southcentralus"
+$ResourceGroupName = "RG-JIT-Demo"
+$VMName = "Target"
 # Please make sure that the duration between the request's start and end times is less than 24 hours and higher than 5 minutes.
 $JITAccessDuration = "PT5M"
 $JITAccessPort = 22
-$allowedSourceAddressPrefix = "47.149.128.121"
-####################################################################
-
-####################################################################
-# Retrieve access token for Service Principal from OAuth2 endpoint
-# $TenantId = "4f813308-71b6-4e90-b5db-fa6070c6de3e"
-# $ClientId = "XXXXXX"
-# $ClientSecret = "XXXXXX"
-# $Resource = "https://management.core.windows.net/"
-# $RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
-# $bodyAuth = "grant_type=client_credentials&client_id=$UAMIClientId&client_secret=$ClientSecret&resource=$Resource"
-# $tokenResponse = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $bodyAuth -ContentType 'application/x-www-form-urlencoded'
+$allowedSourceAddressPrefix = "10.0.0.4"
 ####################################################################
 
 # Obtain access token for User Assigned Managed Identity, we need to include client_id of the User Assigned MI in the query
